485243b665481d53621f2e64a8760f705fff3fbc
Inácio Klassmann <inacio@versates.com>
2016-10-26 17:34:36 -0200

    bugfix for heroku deploy

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/arcgis-js-api/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/dojo/index.d.ts" />
+/// <reference path="globals/socket.io-client/index.d.ts" />

b942477a036fbab0b0721c41550285c03942a0f6
Inácio Klassmann <inacio@versates.com>
2016-10-27 01:35:57 -0200

    bugfix for heroku

--- app/socket.service.ts
+++ app/socket.service.ts
@@ -9,10 +9,10 @@ console.log(process);
 @Injectable()
 export class SocketService {
     private socket: SocketIOClient.Socket;
-    private protocol = 'https';
-    private host = 'arcane-mountain-81855.herokuapp.com';
-    private port = '';
-    private namespace = '/map';
+    private protocol = process.env.TARGET_SERVER_PROTOCOL || window.location.protocol;
+    private host = process.env.TARGET_SERVER_HOST || window.location.hostname;
+    private port = process.env.TARGET_SERVER_PORT || 5000;
+    private namespace = process.env.TARGET_SERVER_NAMESPACE || '/map';
 
     constructor() {}
 
@@ -20,7 +20,7 @@ export class SocketService {
         console.log("SOCKET");
         console.log(this.socket);
         if (!this.socket || !this.socket.connected) {
-            let socketUrl = this.protocol + "://" + this.host + this.namespace;
+            let socketUrl = this.protocol + "://" + this.host + ":" + this.port + this.namespace;
             this.socket = io.connect(socketUrl);
             this.socket.on("connect", () => this.connect());
             this.socket.on("disconnect", () => this.disconnect());
